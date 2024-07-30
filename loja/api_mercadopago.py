6177682986400a6df9cb891b9e4e816b35fd4084
weslley <weslleycorreapimenta2002@gmail.comgit config --global user.email weslleycorreapimenta2002@gmail.comgit config --global user.email weslleycorreapimenta2002@gmail.com>
2024-07-30 18:48:16 -0300

    test_comit

--- loja/api_mercadopago.py
+++ loja/api_mercadopago.py
@@ -1,4 +1,8 @@
 import mercadopago
-# git push -u origin master
+public_key =  "APP_USR-dd7b7825-e3d3-47ad-b582-2836ac155d14"
+token = "APP_USR-7769193582522514-073017-6a1a4bf4137e959cda95b77b38fea67c-1925206522"
+
+
+
 
 
