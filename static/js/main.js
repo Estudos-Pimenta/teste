4d9d38db017dc5c062e37ba940063a8935084c05
weslley <weslleycorreapimenta2002@gmail.comgit config --global user.email weslleycorreapimenta2002@gmail.comgit config --global user.email weslleycorreapimenta2002@gmail.com>
2024-08-14 12:24:03 -0300

    Projeto Atualizado e funcionando

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,7 @@
-var baseUrl = new URL(document.URL);
+var url = new URL(document.URL);
 var itens = document.getElementsByClassName("item-ordenar");
 
-for (var i = 0; i < itens.length; i++) {
-    var itemUrl = new URL(baseUrl); // Crie uma nova instância de URL para cada iteração
-    itemUrl.searchParams.set("ordem", itens[i].name);
-    itens[i].href = itemUrl.href;
+for (i = 0; i < itens.length; i++){
+    url.searchParams.set("ordem", itens[i].value);
+    itens[i].value = url.href;
 }
